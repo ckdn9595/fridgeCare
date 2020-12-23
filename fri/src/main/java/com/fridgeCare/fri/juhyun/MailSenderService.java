42e9adf13073f039432ed6990edb342088594417
class02 <class02@host.docker.internal>
2020-12-23 16:51:08 +0900

    commit!

--- fri/src/main/java/com/fridgeCare/fri/juhyun/MailSenderService.java
+++ fri/src/main/java/com/fridgeCare/fri/juhyun/MailSenderService.java
@@ -16,10 +16,10 @@ public class MailSenderService {
 	public void sendAuthMail(String mail, String id) {
 		StringBuffer htmlContent = new StringBuffer();
 		htmlContent.append("안녕하세요 " + id +" 회원님 /n");
-		htmlContent.append("현재 " + id + " 회원님은 최종 로그인 시간으로부터 1년의 시간이 경과하여 /n");
-		htmlContent.append("냉장고를 부탁해 사이트의 개인정보 보호 기간이 만료하고 있음을 알려드리며 /n");
-		htmlContent.append("1주일 안으로 로그인하지 않을시/n");
-		htmlContent.append("회원님과 관련된 모든 정보가 삭제됨을 알려드립니다./n");
+		htmlContent.append("현재 " + id + " 회원님은 최종 로그인 시간으로부터 1년의 시간이 경과하여 <br>");
+		htmlContent.append("냉장고를 부탁해 사이트의 개인정보 보호 기간이 만료하고 있음을 알려드리며 <br>");
+		htmlContent.append("1주일 안으로 로그인하지 않을시<br>");
+		htmlContent.append("회원님과 관련된 모든 정보가 삭제됨을 알려드립니다.<br>");
 		
 		try {
 			MailUtil sendMail = new MailUtil(mailSender);
