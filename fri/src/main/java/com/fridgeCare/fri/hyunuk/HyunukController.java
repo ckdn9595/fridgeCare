56f22459d7b863f57fbb5f423546085283f739fc
gogorailgun <gogorailgun@naver.com>
2020-12-24 12:15:46 +0900

    search commit

--- fri/src/main/java/com/fridgeCare/fri/hyunuk/HyunukController.java
+++ fri/src/main/java/com/fridgeCare/fri/hyunuk/HyunukController.java
@@ -1,9 +1,95 @@
 package com.fridgeCare.fri.hyunuk;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fridgeCare.fri.hyunuk.util.PageUtil;
+import com.fridgeCare.fri.hyunuk.vo.GetVO;
+import com.fridgeCare.fri.hyunuk.vo.IngredVO;
+import com.fridgeCare.fri.hyunuk.vo.SearchVO;
+
+
 @Controller
+@RequestMapping("/hyunuk")
 public class HyunukController {
 	
+	@Autowired
+	SearchDao sDao;
+	
+	// 페이지 요청
+	@RequestMapping("/search.fri")
+	public ModelAndView getList(ModelAndView mv, PageUtil page, SearchVO sVO, IngredVO inVO, HttpSession session) {
+		mv.setViewName("hyunuk/search");
+				
+		List<IngredVO> ingred = sDao.getIngredient(inVO);
+		mv.addObject("LIST", ingred);
+		return mv;
+	}
+	
+	// 선택된 값 요청
+	@RequestMapping(value="/selected.fri", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> SelectedIngred(ModelAndView mv, String[] Situat, String[] Category, String SelIng) {
+		
+		String situ = "";
+		String CAT = "";
+		
+		if(Situat == null || Situat.length == 0) {
+			situ = "'SELF','DRINK','SIDE','FULL'";
+		}else {
+			for(int i = 0; i < Situat.length; i++) {
+				situ += "'"+(String)Situat[i] + "',";
+			}
+			situ = situ.substring(0,situ.length()-1);
+		}
+		
+		if(Category == null || Category.length == 0) {
+			CAT = "'KR','CH','JP','WE','FU'";
+		}else {
+			for(int i = 0; i < Category.length; i++) {
+				CAT += "'"+(String)Category[i] + "',";
+			}
+			CAT = CAT.substring(0,CAT.length()-1);
+		}
+		
+		if(SelIng == "") {
+			SelIng = "|";
+		}
+		
+		GetVO gVO = new GetVO();
+		gVO.setCat(CAT);
+		gVO.setSitu(situ);
+		gVO.setSeling(SelIng);
+		
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaa");
+		System.out.println(gVO.getCat());
+		System.out.println(gVO.getSitu());
+		System.out.println(gVO.getSeling());
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("CAT", CAT);
+		map.put("situ", situ);
+		map.put("SelIng", SelIng);
+		
+		// List<SearchVO> sLIST = sDao.getRecipelist(gVO);
+		List<SearchVO> sLIST = sDao.getRecipelist(map);
+		
+		System.out.println(sLIST.size());
+		
+		Map<String,Object> map2 = new HashMap<String, Object>();
+		map2.put("sLIST",sLIST);
+		return map2;
+	}
 }
--- fri/src/main/java/com/fridgeCare/fri/hyunuk/SearchDao.java
+++ fri/src/main/java/com/fridgeCare/fri/hyunuk/SearchDao.java
@@ -1,10 +1,41 @@
 package com.fridgeCare.fri.hyunuk;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fridgeCare.fri.hyunuk.vo.GetVO;
+import com.fridgeCare.fri.hyunuk.vo.IngredVO;
+import com.fridgeCare.fri.hyunuk.vo.SearchVO;
 
 public class SearchDao {
 	@Autowired
 	SqlSessionTemplate sqlSession;
 	
+	// 게시물 조회
+	public List<SearchVO> getRecipelist(Map<String, String> map){
+		return sqlSession.selectList("sSQL.getRecipelist", map);
+	}
+
+	// 재료 채워주는 함수
+	public int getData(GetVO gVO) {
+		return sqlSession.selectOne("sSQL.getIngredient");
+	}
+	
+	// 재료 전부 조회 
+	public List<IngredVO> getIngredient(IngredVO inVO) {
+		return sqlSession.selectList("sSQL.getIngredient", inVO);
+	}
+	
+	// 타이틀 검색
+	public List<SearchVO> titleSearch(SearchVO sVO) {
+		return sqlSession.selectList("sSQL.TitleSearch", sVO);
+	}
+	
+	// 조회수 증가
+	public int increseCnt(int bno) {
+		return sqlSession.update("sSQL.increseCnt");
+	}
 }
